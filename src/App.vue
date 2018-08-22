4c83f623cd38a1aa8c363aa95cc345e0ed948c67
Justin Navarro <info@justinnavarro.net>
2018-08-22 15:23:02 -0400

    little bit of clean up and created login page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<v-app>
+			<div id="nav">
+				<router-link to="/">Home</router-link> |
+				<router-link to="/about">About</router-link> |
+				<router-link to="/login">Login</router-link>
+			</div>
+			<router-view/>
+		</v-app>
+	</div>
 </template>
 
 <style lang="scss">
---
+++ src/pages/Login.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="login">
+		Login works...
+	</div>
+</template>
+
+<script lang="ts">
+	import { Component, Vue } from 'vue-property-decorator';
+
+	@Component({})
+	class Login extends Vue {}
+
+	export default Login;
+</script>
+
+<style lang="scss" scoped>
+
+</style>
