370b6c72dcf264784c875b7d52a8401c1c5a4135
Justin Navarro <info@justinnavarro.net>
2019-01-07 21:17:02 -0500

    update store with latest

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,31 +1,14 @@
-import axios, {AxiosResponse} from 'axios';
-import {env} from '@/env.config';
 import Vue from 'vue';
 import Vuex from 'vuex';
 import User from './modules/user.store';
-import {UrlInterface} from '@/interfaces/url.interface';
 import { cookiesService } from '@/module';
 
 Vue.use(Vuex);
 
-const userCookieName: string = env.cookie.name;
+const userCookieName: any = process.env.VUE_APP_TOKEN;
 
 export default new Vuex.Store({
-    actions: {
-        async authGet({ commit }: any, data: UrlInterface ): Promise<AxiosResponse> {
-            const cookie = cookiesService.getCookie(userCookieName);
-            return await axios({
-                method: 'GET',
-                url: env.api.domain + data.url,
-                data: {
-                    params: {},
-                },
-                headers: {
-                    Authorization: `Bearer ${cookie}`,
-                },
-            });
-        },
-    },
+    actions: {},
     mutations: {
         logUserOut(state: any) {
             const excludeDefault: string[] = ['BusinessTypes'];
